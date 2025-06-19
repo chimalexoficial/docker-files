e8e62ce6da98a3896dc1abb0f7ee23d84926202f
chimalexoficial <chimalexoficial@gmail.com>
2025-06-19 11:38:44 -0600

    GET method updated

--- 09-key-value-project/backend/src/routes/store.js
+++ 09-key-value-project/backend/src/routes/store.js
@@ -1,20 +1,66 @@
 const express = require('express');
+const { KeyValue } = require('../models/keyValue')
 
 const keyValueRouter = express.Router();
 
-keyValueRouter.post('/', (req, res) => {
-    return res.send('Creating key-value pair')
+keyValueRouter.post('/', async (req, res) => {
+    const { key, value } = req.body;
+
+    if (!key || !value) {
+        return res.status(400).json({ error: 'Both key and value are required' });
+    }
+
+    try {
+        const existingKey = await KeyValue.findOne({ key });
+        if (existingKey) {
+            return res.status(400).json(
+                {
+                    error: 'Key already exists'
+                })
+        }
+
+        const keyValue = new KeyValue({ key, value });
+        await keyValue.save();
+        return res.status(201).json({ message: 'Key-value pair stored succesfully' });
+
+    } catch (err) {
+        res.status(500).json({ message: 'Internal server error' });
+    }
 });
 
-keyValueRouter.get('/:key', (req, res) => {
+keyValueRouter.get('/:key', async (req, res) => {
+    const { key } = req.params;
+
+    try {
+        const keyValue = await KeyValue.findOne({ key });
+        if (!keyValue) {
+            return res.status(404).json({ error: 'key not found :(' })
+        }
+
+        return res.status(200).json({ key: keyValue.key, value: keyValue.value });
+
+
+    } catch (err) {
+        res.status(500).json({ message: 'Internal server error' })
+    }
     return res.send('Getting key-value pair')
 });
 
 keyValueRouter.put('/:key', (req, res) => {
+    try {
+
+    } catch (err) {
+        res.status(500).json({ message: 'Internal server error' })
+    }
     return res.send('Updating key-value pair')
 });
 
 keyValueRouter.delete('/:key', (req, res) => {
+    try {
+
+    } catch (err) {
+        res.status(500).json({ message: 'Internal server error' })
+    }
     return res.send('Deleting key-value pair')
 
 });

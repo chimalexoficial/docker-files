e74d8ed701b55d107fb2ee2946dea454cc22707e
chimalexoficial <chimalexoficial@gmail.com>
2025-06-19 12:05:27 -0600

    PUT & DELETE added and working

--- 09-key-value-project/backend/src/routes/store.js
+++ 09-key-value-project/backend/src/routes/store.js
@@ -46,23 +46,46 @@ keyValueRouter.get('/:key', async (req, res) => {
     return res.send('Getting key-value pair')
 });
 
-keyValueRouter.put('/:key', (req, res) => {
+keyValueRouter.put('/:key', async (req, res) => {
+    const { key } = req.params;
+    const { value } = req.body;
+
+    if (!value) {
+        return res.status(400).json({ error: 'value is required' });
+    }
+
     try {
+        const keyValue = await KeyValue.findOneAndUpdate({ key },
+            { value },
+            { new: true }
+        );
+
+        if (!keyValue) {
+            return res.status(404).json({ error: 'Key not found' });
+        };
+
+        return res.status(200).json({ message: 'key-value pair updated successfully :)', key: keyValue.key, value: keyValue.value });
 
     } catch (err) {
         res.status(500).json({ message: 'Internal server error' })
     }
-    return res.send('Updating key-value pair')
 });
 
-keyValueRouter.delete('/:key', (req, res) => {
+keyValueRouter.delete('/:key', async (req, res) => {
+    const { key } = req.params;
+
     try {
+        const keyValue = await KeyValue.findOneAndDelete({ key });
+
+        if (!keyValue) {
+            return res.status(404).json({ error: 'Key not found :(' })
+        }
+
+        return res.status(200).json({ message: 'key value pair deleted' });
 
     } catch (err) {
         res.status(500).json({ message: 'Internal server error' })
     }
-    return res.send('Deleting key-value pair')
-
 });
 
 module.exports = {

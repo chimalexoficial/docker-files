50d30347951113e23aeadacfedb02177b51472fa
chimalexoficial <chimalexoficial@gmail.com>
2025-06-24 10:50:44 -0600

    PUT & DELETE added

--- 11-notes-rest-api/notebooks-backend/src/routes.js
+++ 11-notes-rest-api/notebooks-backend/src/routes.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const notebookRouter = express.Router();
-const { Notebook } = require('./models')
+const mongoose = require('mongoose');
+const { Notebook } = require('./models');
+
 
 // Create new notebooks: POST '/'
 notebookRouter.post('/', async (req, res) => {
@@ -33,20 +35,80 @@ notebookRouter.get('/', async (req, res) => {
             error: error.message
         })
     }
-})
+});
 // Get a single notebook: GET '/:id' - localhost:8080/api/notebooks/'the-id'
 notebookRouter.get('/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
 
+        if (!mongoose.Types.ObjectId.isValid(id)) {
+            return res.status(404).json({ error: 'Notebook not found' });
+        }
+        const notebook = await Notebook.findById(id);
+
+        if (!notebook) {
+            return res.status(404).json({ error: 'Notebook not found' });
+        }
+        return res.status(200).json({ data: notebook })
+
+    } catch (error) {
+        res.status(500).json({
+            error: error.message
+        })
+    }
 });
 
 // Update a single notebook: PUT '/:id' - localhost:8080/api/notebooks/'the-id'
 notebookRouter.put('/:id', async (req, res) => {
+    try {
+        const { name, description } = req.body;
+        const { id } = req.params;
 
+        if (!mongoose.Types.ObjectId.isValid(id)) {
+            return res.status(404).json({ error: 'Notebook not found' });
+        }
+
+        const notebook = await Notebook.findByIdAndUpdate(
+            id,
+            { name, description },
+            { new: true }
+        );
+
+        if (!notebook) {
+            return res.status(404).json({ error: 'Notebook not found to update' });
+        }
+
+        return res.json({ data: notebook })
+
+    } catch (error) {
+        res.status(500).json({
+            error: error.message
+        })
+    }
 });
 
 // Delete a single notebook: DELETE '/:id' - localhost:8080/api/notebooks/'the-id'
 notebookRouter.delete('/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
 
+        if (!mongoose.Types.ObjectId.isValid(id)) {
+            return res.status(404).json({ error: 'Notebook not found' });
+        }
+
+        const notebook = await Notebook.findByIdAndDelete(id);
+
+        if (!notebook) {
+            return res.status(404).json({ error: 'Notebook not found to delete' });
+        }
+
+        return res.sendStatus(204);
+
+    } catch (error) {
+        res.status(500).json({
+            error: error.message
+        })
+    }
 });
 
 module.exports = {
